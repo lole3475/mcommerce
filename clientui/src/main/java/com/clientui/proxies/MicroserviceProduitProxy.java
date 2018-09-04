997adb37540b682187cafa64ff782319023a4952
Laureen3475 <laureen.vuillaume@yahoo.fr>
2018-09-04 15:28:24 +0200

    Projet avec Ribbon

--- clientui/src/main/java/com/clientui/proxies/MicroserviceProduitProxy.java
+++ clientui/src/main/java/com/clientui/proxies/MicroserviceProduitProxy.java
@@ -2,14 +2,15 @@ package com.clientui.proxies;
 
 
 import com.clientui.beans.ProductBean;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 import java.util.Optional;
-
-@FeignClient(name = "microservice-produits", url="localhost:9001")
+@RibbonClient(name = "microservice-produits")
+@FeignClient(name = "microservice-produits")
 public interface MicroserviceProduitProxy {
     @GetMapping(value = "/Produits")
     List<ProductBean> listeDesProduits();
