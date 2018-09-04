4133404ff1e08cddba5a5eb8493f07d60ea3b5d1
Laureen3475 <laureen.vuillaume@yahoo.fr>
2018-09-04 14:08:14 +0200

    Projet avec Eureka

--- microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
+++ microservice-commandes/src/main/java/com/mcommandes/McommandesApplication.java
@@ -3,8 +3,11 @@ package com.mcommandes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class McommandesApplication {
 
 	public static void main(String[] args) {
--- microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
+++ microservice-paiement/src/main/java/com/mpaiement/MpaiementApplication.java
@@ -2,10 +2,14 @@ package com.mpaiement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients("com.mpaiement")
+@EnableDiscoveryClient
+@EnableConfigurationProperties
 public class MpaiementApplication {
 
 	public static void main(String[] args) {
